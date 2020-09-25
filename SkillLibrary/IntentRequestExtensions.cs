4afb1efa393d3cb94af9f3b0c75f5d183dee23ac
sdg123 <sgonsal@ncsu.edu>
2020-09-25 14:57:53 -0400

    Enable basic reminders functionality

--- SkillLibrary/IntentRequestExtensions.cs
+++ SkillLibrary/IntentRequestExtensions.cs
@@ -10,9 +10,11 @@ namespace SkillLibrary
         public static readonly string GetGroceries = "GetGroceryItemsIntent";
         public static readonly string AddGroceries = "AddGroceryItemIntent";
         public static readonly string FinishedShopping = "ArchiveAllCardsIntent";
+        public static readonly string AmazonYes = "AMAZON.YesIntent";
 
         public static bool IsGetGroceries(this IntentRequest intentRequest) => intentRequest.Intent.Name.Equals(GetGroceries);
         public static bool IsAddGroceries(this IntentRequest intentRequest) => intentRequest.Intent.Name.Equals(AddGroceries);
         public static bool IsFinishedShopping(this IntentRequest intentRequest) => intentRequest.Intent.Name.Equals(FinishedShopping);
+        public static bool Yes(this IntentRequest intentRequest) => intentRequest.Intent.Name.Equals(AmazonYes);
     }
 }
