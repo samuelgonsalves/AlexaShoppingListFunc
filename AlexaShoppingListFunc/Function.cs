048fd1db843a181d610a01e46becf28f0e1b2694
sdg123 <sgonsal@ncsu.edu>
2020-10-05 12:52:50 -0400

    Add a weekly reminder on Saturdays

--- AlexaShoppingListFunc/Function.cs
+++ AlexaShoppingListFunc/Function.cs
@@ -35,94 +35,104 @@ namespace AlexaShoppingListFunc
 
         public static async Task<SkillResponse> FunctionHandlerAsync(SkillRequest input, ILambdaContext context)
         {
-            if (input.Context.System.User.Permissions == null)
+            try
             {
-                return ResponseBuilder.TellWithAskForPermissionConsentCard("To set reminders, you need to allow Trello Grocery to create and edit reminders for this skill. Would you like to do that?",
-                                                                            new[] { "alexa::alerts:reminders:skill:readwrite" }, input.Session
-                                                                          );
-            }
-            else
-            {
-                // Has permissions, ask if user wants to create a reminder?
-
-                var sess = input.Session;
-
-                if (!await RemindersExtensions.AnyActiveRemindersAsync(input) 
-                    && sess.Attributes == null)
+                if (input.Context.System.User.Permissions == null)
                 {
-                    sess.Attributes = new Dictionary<string, object>();
-                    sess.Attributes["reminderRequest"] = true;
-
-                    return ResponseBuilder.Ask("Would you like to be reminded to get your groceries weekly?", new Reprompt("I'm sorry, I didn't catch that. Could you repeat it?"), sess);
+                    return ResponseBuilder.TellWithAskForPermissionConsentCard("To set reminders, you need to allow Trello Grocery to create and edit reminders for this skill. Would you like to do that?",
+                                                                                new[] { "alexa::alerts:reminders:skill:readwrite" }, input.Session
+                                                                              );
                 }
-            }
-
-            if (input.IsIntentRequest())
-            {
-                var intentRequest = input.Request as IntentRequest;
-
-                // TODO: Figure out how to find list without hardcoding listId
-                if (intentRequest.IsAddGroceries())
+                else
                 {
-                    var item = intentRequest.Intent.Slots["GroceryItem"].SlotValue;
+                    // Has permissions, ask if user wants to create a reminder?
 
-                    if (item.SlotType == SlotValueType.List)
+                    var sess = input.Session;
+
+                    if (!await RemindersExtensions.AnyActiveRemindersAsync(input)
+                        && sess.Attributes == null)
                     {
-                        foreach (var v in item.Values)
+                        sess.Attributes = new Dictionary<string, object>();
+                        sess.Attributes["reminderRequest"] = true;
+
+                        return ResponseBuilder.Ask("Would you like to be reminded to get your groceries weekly?", new Reprompt("I'm sorry, I didn't catch that. Could you repeat it?"), sess);
+                    }
+                }
+
+                if (input.IsIntentRequest())
+                {
+                    var intentRequest = input.Request as IntentRequest;
+
+                    // TODO: Figure out how to find list without hardcoding listId
+                    if (intentRequest.IsAddGroceries())
+                    {
+                        var item = intentRequest.Intent.Slots["GroceryItem"].SlotValue;
+
+                        if (item.SlotType == SlotValueType.List)
                         {
-                            await _trelloApiClient.Cards.CreateCardAsync(new Trello.Responses.Card { ListId = "5ed532f0b58ba36765935ff3", Name = v.Value.CapitalizeFirstLetterOfEachWord() });
+                            foreach (var v in item.Values)
+                            {
+                                await _trelloApiClient.Cards.CreateCardAsync(new Trello.Responses.Card { ListId = "5ed532f0b58ba36765935ff3", Name = v.Value.CapitalizeFirstLetterOfEachWord() });
+                            }
+
+                            return ResponseBuilder.Tell($"I've added {item.Values.Count()} items to your grocery board.");
                         }
 
-                        return ResponseBuilder.Tell($"I've added {item.Values.Count()} items to your grocery board.");
+                        await _trelloApiClient.Cards.CreateCardAsync(new Trello.Responses.Card { ListId = "5ed532f0b58ba36765935ff3", Name = item.Value.CapitalizeFirstLetterOfEachWord() });
+
+                        return ResponseBuilder.Tell($"I've added {item} to your grocery board");
                     }
-
-                    await _trelloApiClient.Cards.CreateCardAsync(new Trello.Responses.Card { ListId = "5ed532f0b58ba36765935ff3", Name = item.Value.CapitalizeFirstLetterOfEachWord() });
-
-                    return ResponseBuilder.Tell($"I've added {item} to your grocery board");
-                }
-                else if (intentRequest.IsGetGroceries())
-                {
-                    var cardsOnGroceryList = await _trelloApiClient.Lists.GetCards("5ed532f0b58ba36765935ff3");
-                    return ResponseBuilder.Tell(SkillResponseSentences.GetGroceryItems(cardsOnGroceryList));
-                }
-                else if (intentRequest.IsFinishedShopping())
-                {
-                    // Archive all cards on the Already Have list
-                    // TODO: Figure out how to archive list without hardcoding listId
-                    await _trelloApiClient.Lists.ArchiveAllCards("5ed65088014d693574335aec");
-
-                    return SkillResponseExtensions.GetFinishedShoppingIntentResponse();
-                }
-                else if (intentRequest.Yes())
-                {
-                    if (input.Session.Attributes == null)
-                        throw new Exception("No attributes specified for YES intent");
-
-                    if (input.Session.Attributes["reminderRequest"].Equals(true))
+                    else if (intentRequest.IsGetGroceries())
                     {
-                        var response = await RemindersExtensions.CreateReminder(
-                                   input,
-                                   new Reminder
-                                   {
-                                       RequestTime = DateTime.UtcNow,
-                                       AlertInformation = new AlertInformation(new[] { new SpokenContent("Get your groceries!", "en-GB") }),
-                                       Trigger = new RelativeTrigger(120), // 2 minutes
-                                       PushNotification = PushNotification.Enabled
-                                   }
-                               );
-
-                        return ResponseBuilder.Tell("I've added a reminder.");
+                        var cardsOnGroceryList = await _trelloApiClient.Lists.GetCards("5ed532f0b58ba36765935ff3");
+                        return ResponseBuilder.Tell(SkillResponseSentences.GetGroceryItems(cardsOnGroceryList));
                     }
-                    
+                    else if (intentRequest.IsFinishedShopping())
+                    {
+                        // Archive all cards on the Already Have list
+                        // TODO: Figure out how to archive list without hardcoding listId
+                        await _trelloApiClient.Lists.ArchiveAllCards("5ed65088014d693574335aec");
+
+                        return SkillResponseExtensions.GetFinishedShoppingIntentResponse();
+                    }
+                    else if (intentRequest.Yes())
+                    {
+                        if (input.Session.Attributes == null)
+                            throw new Exception("No attributes specified for YES intent");
+
+                        if (input.Session.Attributes["reminderRequest"].Equals(true))
+                        {
+                            var reminder = new Reminder
+                            {
+                                RequestTime = DateTime.UtcNow,
+                                AlertInformation = new AlertInformation(new[] { new SpokenContent("Get your groceries!", "en-GB") }),
+                                Trigger = new AbsoluteTrigger(Convert.ToDateTime(DateTime.Now.AddMinutes(1).ToString("yyyy-MM-ddTHH:mm:ss.fff")),
+                                                              new Recurrence("WEEKLY", new List<string> { "SA" })),
+                                PushNotification = PushNotification.Enabled
+                            };
+
+                            var response = await RemindersExtensions.CreateReminder(
+                                                        input,
+                                                        reminder
+                                                    );
+
+                            return ResponseBuilder.Tell("I'll remind you to get your grocery every Saturday.");
+                        }
+
+                    }
+                }
+                else if (input.IsLaunchRequest())
+                {
+                    return SkillResponseExtensions.GetLaunchResponse();
+                }
+                else if (input.IsSessionEndedRequest())
+                {
+                    return SkillResponseExtensions.GetSessionEndedResponse();
                 }
             }
-            else if (input.IsLaunchRequest())
+            catch(Exception ex)
             {
-                return SkillResponseExtensions.GetLaunchResponse();
-            }
-            else if (input.IsSessionEndedRequest())
-            {
-                return SkillResponseExtensions.GetSessionEndedResponse();
+                context.Logger.Log(ex.Message);
             }
 
             return ResponseBuilder.Tell("Skill default response");
